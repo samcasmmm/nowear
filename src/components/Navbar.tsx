5262ac055b665ccdaf6287904f9c54e17c73f385
Sameer Bagwan <87710112+samcasmmm@users.noreply.github.com>
2024-04-22 14:14:42 +0000

    minor fix

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,11 +1,11 @@
-'use client';
-import React, { useState } from 'react';
-import Icon from '@/components/Icon';
-import { Baby, Search, ShoppingBag, ShoppingBasket } from 'lucide-react';
-import { motion } from 'framer-motion';
-import Image from 'next/image';
-import Link from 'next/link';
-import { cn } from '@/lib/utils';
+"use client";
+import React, { useState } from "react";
+import Icon from "@/components/Icon";
+import { Baby, Search, ShoppingBag, ShoppingBasket } from "lucide-react";
+import { motion } from "framer-motion";
+import Image from "next/image";
+import Link from "next/link";
+import { cn } from "@/lib/utils";
 import {
   Sheet,
   SheetContent,
@@ -13,26 +13,26 @@ import {
   SheetHeader,
   SheetTitle,
   SheetTrigger,
-} from '@/components/ui/sheet';
-import NavigationLinks from '@/components/navbar/Navigation';
+} from "@/components/ui/sheet";
+import NavigationLinks from "@/components/navbar/Navigation";
 
 type Props = {};
 
 const navLinks = [
-  { href: '/men', label: 'Men' },
-  { href: '/women', label: 'Women' },
-  { href: '/summer-tee', label: 'Summer T-Shirt' },
-  { href: '/oversized-tee', label: 'Oversized T-Shirt' },
+  { href: "/men", label: "Men" },
+  { href: "/women", label: "Women" },
+  { href: "/summer-tee", label: "Summer T-Shirt" },
+  { href: "/oversized-tee", label: "Oversized T-Shirt" },
 ];
 
 const BrandLogo = () => (
-  <Link href={'/'}>
+  <Link href={"/"}>
     <Image
-      src={'/images/logo_light.png'}
-      alt='Logo'
+      src={"/images/logo_light.png"}
+      alt="Logo"
       width={120}
       height={100}
-      className='cursor-pointer'
+      className="cursor-pointer"
     />
   </Link>
 );
@@ -42,8 +42,8 @@ const NavLinks: React.FC<NavLinksProps> = ({ href, label, className }) => (
   <Link
     href={href}
     className={cn(
-      'hover:opacity-100 opacity-60 transition-opacity duration-300',
-      className
+      "hover:opacity-100 opacity-60 transition-opacity duration-300",
+      className,
     )}
   >
     {label}
@@ -53,72 +53,72 @@ const NavLinks: React.FC<NavLinksProps> = ({ href, label, className }) => (
 const SearchBar: React.FC<{ className?: string }> = ({ className }) => (
   <div
     className={cn(
-      'border flex items-center justify-center flex-row p-2 rounded-sm',
-      className
+      "border flex items-center justify-center flex-row p-2 rounded-sm",
+      className,
     )}
   >
-    <Icon name='Search' />
-    <input type='text' className=' outline-none pl-2 w-full h-full' />
+    <Icon name="Search" />
+    <input type="text" className=" outline-none pl-2 w-full h-full" />
   </div>
 );
 
 const Navbar = (props: Props) => {
   return (
     <Sheet>
-      <div className='border-b border-b-gray-100 flex items-center justify-center px-3 relative'>
-        <div className='fluid flex items-center justify-between py-5'>
-          <div className='flex items-center justify-between md:justify-start p-2 gap-10 font-bold w-4/6'>
-            <SheetTrigger className='inline-block sm:hidden'>
-              <Icon name='Menu' />
+      <div className="border-b border-b-gray-100 flex items-center justify-center px-3 relative">
+        <div className="fluid flex items-center justify-around py-5">
+          <div className="flex items-center justify-between md:justify-start p-2 gap-10 font-bold w-4/6">
+            <SheetTrigger className="inline-block sm:hidden">
+              <Icon name="Menu" />
             </SheetTrigger>
             <BrandLogo />
             <NavigationLinks />
           </div>
-          <div className='flex items-center justify-center gap-4 flex-row'>
-            <SearchBar className='hidden md:flex' />
+          <div className="flex items-center justify-center gap-4 flex-row">
+            <SearchBar className="hidden md:flex" />
             <Icon
-              name='User'
-              className='hover:opacity-100 opacity-60 transition-opacity duration-300'
+              name="User"
+              className="hover:opacity-100 opacity-60 transition-opacity duration-300"
             />
             <Icon
-              name='ShoppingBag'
-              className='hover:opacity-100 opacity-60 transition-opacity duration-300'
+              name="ShoppingBag"
+              className="hover:opacity-100 opacity-60 transition-opacity duration-300"
             />
           </div>
         </div>
       </div>
-      <SheetContent side={'left'}>
+      <SheetContent side={"left"}>
         <SheetHeader>
-          <SheetTitle className='flex items-center justify-center'>
+          <SheetTitle className="flex items-center justify-center">
             <BrandLogo />
           </SheetTitle>
-          <SheetDescription className=''>
-            <div className='flex sm:hidden items-start justify-center space-y-6 flex-col font-bold mt-4'>
+          <SheetDescription className="">
+            <div className="flex sm:hidden items-start justify-center space-y-6 flex-col font-bold mt-4">
               {navLinks.map((link, index) => (
                 <NavLinks
                   href={link.href}
                   label={link.label}
                   key={index}
-                  className='border-b border-black/25 w-full text-left text-AstronautBlue-500 hover:text-AstronautBlue-600'
+                  className="border-b border-black/25 w-full text-left text-AstronautBlue-500 hover:text-AstronautBlue-600"
                 />
               ))}
-              <div className='flex items-start justify-center w-full space-x-6'>
-                <div className='flex items-start justify-between flex-col text-left w-full space-y-4'>
-                  <p className='text-left cursor-pointer font-normal'>
+              <div className="flex items-start justify-center w-full space-x-6">
+                <div className="flex items-start justify-between flex-col text-left w-full space-y-4">
+                  <p className="text-left cursor-pointer font-normal">
                     Track Order
                   </p>
-                  <p className='text-left cursor-pointer font-normal'>
+                  <p className="text-left cursor-pointer font-normal">
                     Reviewa
                   </p>
                 </div>
-                <div className='flex items-start justify-between flex-col w-full space-y-4'>
-                  <p className='text-left cursor-pointer font-normal'>
+                <div className="flex items-start justify-between flex-col w-full space-y-4">
+                  <p className="text-left cursor-pointer font-normal">
                     Support
                   </p>
-                  <p className='text-left cursor-pointer font-normal'>
+                  <p className="text-left cursor-pointer font-normal">
                     Return & Exchange
                   </p>
-                  <p className='text-left cursor-pointer font-normal'>
+                  <p className="text-left cursor-pointer font-normal">
                     Contact Us
                   </p>
                 </div>
