44606ddf834fd50f8f6b2b554ab892fe636c001d
Sameer Bagwan <sameerbagwan132@gmail.com>
2024-04-27 15:23:11 +0530

    revamp

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,31 +1,9 @@
 'use client';
 
-import Carousel from '@/components/carousel/Carousel';
-import Header from '@/components/Header';
-import Navbar from '@/components/Navbar';
 import Image from 'next/image';
-import carouselData from '@/data/coursel.json';
-import NewCarousel from '@/components/carousel/NewCarousel';
 import useDeviceSize from '@/hooks/useDeviceSize';
-
-const imgs = [
-  { image: '/images/image1.png', alt: 'asd' },
-  { image: '/images/image2.png', alt: 'asd' },
-];
 
 export default function Home() {
   const [width, height] = useDeviceSize();
-  return (
-    <main>
-      <Header />
-      <Navbar />
-      <div className='w-full h-full sm:container sm:py-6'>
-        <NewCarousel>
-          {imgs.map((img) => (
-            <Image src={img.image} width={width} height={height} alt='banner' />
-          ))}
-        </NewCarousel>
-      </div>
-    </main>
-  );
+  return <main></main>;
 }
